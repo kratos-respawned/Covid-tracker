ef5a61ce2a19d6ff205af54f683a51c9335b0cb5
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-05-07 17:39:21 +0530

    fixed news cards and added backend url

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -3,6 +3,7 @@ import { News } from "../../../typings/news";
 import { Separator } from "@/components/ui/separator";
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
@@ -15,10 +16,16 @@ async function page() {
   return (
     <>
       <Separator />
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-        {data.map((news, index) => (
-          <News {...news} key={index} />
-        ))}
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mt-8">
+        {data
+          .filter((news) => !news.image.endsWith("comundefined"))
+          .filter(
+            (news, index, self) =>
+              index === self.findIndex((t) => t.title === news.title)
+          )
+          .map((news, index) => (
+            <News {...news} key={index} />
+          ))}
       </section>
     </>
   );
@@ -26,9 +33,8 @@ async function page() {
 
 export default page;
 const News = (news: News) => {
-  if (news.image.endsWith("comundefined")) return null;
   return (
-    <Card className="flex flex-col gap-2">
+    <Card className="flex flex-col  gap-2">
       <CardHeader>
         <CardTitle className=" hover:underline underline-offset-4 text-xl capitalize">
           <a href={news.url} target="_blank" rel="noopener noreferrer">
@@ -39,12 +45,13 @@ const News = (news: News) => {
           {news.description.slice(0, 100) + "..."}
         </CardDescription>
       </CardHeader>
-      <img
-        src={news.image}
-        alt={news.title}
-        className=" w-fit h-60 m-6  mt-0 object-cover"
-        height={400}
-      />
+      <CardContent className="overflow-clip group w-full">
+        <img
+          src={news.image}
+          alt={news.title}
+          className=" w-full group-hover:scale-105 transition-transform   mx-auto  object-cover"
+        />
+      </CardContent>
     </Card>
   );
 };
